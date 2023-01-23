88b45aaba1ca94141dd47cb9db5f98b207c50d55
oh <dhdbsgn111@naver.com>
2023-01-23 19:17:24 +0900

    Initial commit

---
+++ src/test/java/oh/study/gitstudy/GitstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package oh.study.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
